dfbcf4c32d9cf0efb70908b74577c6850cb16cdb
--replace-all <adeiji@yahoo.com>
2018-04-01 22:22:00 -0700

    app.all now is reached for all endpoints

--- code/node.js
+++ code/node.js
@@ -24,7 +24,7 @@ function start ()
 
   app.use(bodyParser.json());
 
-  app.all('/', function(req, res, next) {
+  app.all('*', function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
       res.header("Access-Control-Allow-Headers", "Content-Type");
