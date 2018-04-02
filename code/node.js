80290dad452b6ce3f0abde8b70ba8afa111521e3
--replace-all <adeiji@yahoo.com>
2018-04-01 21:44:03 -0700

    Updated access-control-allow-origin

--- code/node.js
+++ code/node.js
@@ -26,8 +26,11 @@ function start ()
 		app.use(app.router);
 
 		app.all('/', function(req, res, next) {
-  			res.header("Access-Control-Allow-Origin", "*");
-  			res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.setHeader('Access-Control-Allow-Credentials', false);
+
   			next();
  		});
 	});
